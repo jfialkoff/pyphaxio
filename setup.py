3822e7f68db172659c6688c5040ea47842618e73
FabioFleitas <cubanfabio@gmail.com>
2015-09-17 00:10:42 -0700

    Moved tests to top level directory & updated setup.py to include a test_suite

--- setup.py
+++ setup.py
@@ -1,23 +1,18 @@
 from setuptools import setup, find_packages
 
-
 setup(
-
-    name = "pyphaxio",
-
-    version = '0.9',
-
-    packages = find_packages(),
-
-    install_requires = ['requests'],
-    include_package_data = True,
+    name='pyphaxio',
+    version='0.9',
+    packages=find_packages(),
+    test_suite='tests',
+    install_requires=['requests'],
+    include_package_data=True,
 
     # metadata for upload to PyPI
-    author = "Joshua Fialkoff",
-    author_email = "joshua.fialkoff@setaris.com",
-    description = "Python client for Phaxio",
-    license = "MIT License",
-    keywords = "python phaxio fax",
-    url = "https://github.com/setaris/pyphaxio",
-
+    author='Joshua Fialkoff',
+    author_email='joshua.fialkoff@setaris.com',
+    description='Python client for Phaxio',
+    license='MIT License',
+    keywords='python phaxio fax',
+    url='https://github.com/setaris/pyphaxio',
 )
